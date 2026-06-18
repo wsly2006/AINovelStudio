f7cab11aea2def93b999b14ca3469f737604f0ba
NewHuman1024 <73478965+wsly2006@users.noreply.github.com>
2026-06-19 02:16:30 +0800

    fix(style-check): bump request timeout to 120s

--- frontend/src/api/chapterStyleChecks.js
+++ frontend/src/api/chapterStyleChecks.js
@@ -4,7 +4,7 @@ export const chapterStyleChecksApi = {
   list: (chapterId) =>
     client.get(`/chapters/${chapterId}/style-checks`).then((r) => r.data),
   create: (chapterId) =>
-    client.post(`/chapters/${chapterId}/style-checks`).then((r) => r.data),
+    client.post(`/chapters/${chapterId}/style-checks`, {}, { timeout: 120000 }).then((r) => r.data),
   remove: (chapterId, checkId) =>
     client.delete(`/chapters/${chapterId}/style-checks/${checkId}`),
 }
